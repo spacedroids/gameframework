c9c29e85a7b359e684d4bd14f4e6614dbba1af89
Ryan Nordman <spacerobots@gmail.com>
2019-02-06 20:40:02 -0800

    Added TitleScreenCanvas class and removed pre-baked GameController objects from Scene hierarchy, so they can be instantiated automatically.

---
+++ Assets/Scripts/TitleScreenCanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenCanvas : MonoBehaviour
+{
+    public void startNewGame()
+    {
+        GameController.Instance.gotoGameplay();
+    }
+}
